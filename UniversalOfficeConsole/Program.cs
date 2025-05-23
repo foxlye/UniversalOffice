5ca69869a2367e614f0a7bc60879e225581eeff4
foxlye <sherstobitovf@mail.ru>
2025-05-24 04:46:39 +0500

    - Update readme . - Add placeholder in console version.

--- UniversalOfficeConsole/Program.cs
+++ UniversalOfficeConsole/Program.cs
@@ -10,6 +10,8 @@ namespace UniversalOfficeConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Project in development.");
+            Console.ReadKey();
         }
     }
 }
